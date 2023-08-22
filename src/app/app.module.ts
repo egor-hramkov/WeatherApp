974cefec2c639929f91ead31fd833c9c1205a236
Egor Khramkov <e.hramkov@ritg.ru>
2023-08-22 16:15:24 +0400

    init homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/ui/layout/layout.component';
+import {HeaderComponent} from "./components/ui/layout/header/header.component";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/ui/layout/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IMenuItem } from "./header.interface";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  menuItems: IMenuItem[] = [
+    {
+      path: '/',
+      icon: 'home'
+    },
+    {
+      path: '/search',
+      icon: 'search'
+    },
+    {
+      path: '/weather',
+      icon: 'thermostat'
+    },
+    {
+      path: '/favorite',
+      icon: 'favorite'
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
